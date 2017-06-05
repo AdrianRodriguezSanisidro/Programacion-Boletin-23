690d1bfbf7cc722c20a6d9a612ab63b5e32ef29e
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-06-05 19:08:11 +0200

    Llamar a los metodos

---
+++ src/boletin/pkg23/Boletin23.java
@@ -0,0 +1,16 @@
+package boletin.pkg23;
+
+/**
+ *
+ * @author Adry
+ */
+public class Boletin23 {
+
+    public static void main(String[] args) {
+        Ventana ven1=new Ventana();
+        ven1.ventana();
+        ven1.insertarPanel1();
+        ven1.insertarPanel2();
+    }
+    
+}
